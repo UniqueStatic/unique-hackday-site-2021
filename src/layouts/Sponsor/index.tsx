42d3b4f135951f8e3cabee509cf8207953ed166b
wangqingyu <wangqingyu.colin@bytedance.com>
2021-05-16 16:03:33 +0800

    feat: sponsor page

---
+++ src/layouts/Sponsor/index.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import computer from '../../assets/imgs/Computer.png';
+import unique from '../../assets/imgs/UniqueMobile.png';
+const pics = {
+  computer,
+  unique,
+};
+
+export { pics };
